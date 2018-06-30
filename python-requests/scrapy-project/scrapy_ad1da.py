884a133ab210f6b7ab3e089c93e980dd51fcf1f5
plusoneee <y02121148s@gmail.com>
2018-06-30 19:53:06 +0800

    w to csv function

--- python-requests/scrapy-project/scrapy_ad1da.py
+++ python-requests/scrapy-project/scrapy_ad1da.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import time
+import csv
 
 def get_all_product_href(paserUrl):
     href_list =[]
@@ -21,38 +22,29 @@ def get_all_product_href(paserUrl):
     del href_list
     return href_list_sorted
 
+# get product detail data
 def paser_products(productHrefList):
     bullets = []
-    product_number = 1
+    yield_data  = []
     for productHref in productHrefList:
         all_size = []
+        # get product ID 
         productId = productHref.split('/',3)[3].replace('.html','')
-        # print(productId)
+        # call api 'https://www.adidas.com/api/products/' <productID>
         prodocutLink =  apiUrl + productId
         response = requests.request("GET", prodocutLink , headers=headers, params='?sitePath=us&region=null')
         product_json_data = json.loads(response.text)
+        # paser Json data
         try:
             product_id = product_json_data["id"]
             model_number = product_json_data["model_number"]
             product_name = product_json_data["name"]
-            Standard_price = product_json_data["pricing_information"]["standard_price"]
+            standard_price = product_json_data["pricing_information"]["standard_price"]
             current_price = product_json_data["pricing_information"]["currentPrice"]
             standard_price_no_vat = product_json_data["pricing_information"]["standard_price_no_vat"]
             description_title = product_json_data["product_description"]["title"]
             description_usps = product_json_data["product_description"]["usps"]
-            # try:
-            #     for line in description_usps:
-            #         if "%" in line: 
-            #             material = line
-            #             break
-            #         elif "Main materials" in line:
-            #             material = line
-            #             break
-            #         else: 
-            #             material += line
-            #             break
-            # except:
-            #     pass
+            # sometime you can't get product subtitle so 
             try:
                 description_subtitle = product_json_data["product_description"]["subtitle"]
             except:
@@ -66,60 +58,78 @@ def paser_products(productHrefList):
             product_type = product_json_data["attribute_list"]["productType"][0]
             image_url = product_json_data["view_list"][0]["image_url"]
         except:
-            print("ERROR :::::::::::::::::::::" ,prodocutLink)
+            print("ERROR :::::::::::::::::::::", prodocutLink)
+        # call secound api rating data:https://www.adidas.com/api/models/<model_number>/ratings?sitePath=us&region=null
         prodocutRateLink =  ratingApi + model_number + '/ratings?sitePath=us&region=null'
+        # paser json data 
         try:
             rate_response = requests.request("GET", prodocutRateLink , headers=headers)
             rate_json_data = json.loads(rate_response.text)
             over_all_rating = rate_json_data["overallRating"]
             recommendation_percentage = rate_json_data["recommendationPercentage"]
             rating_distribution = rate_json_data["ratingDistribution"]
-            # review_number = rate_json_data["ratingDistribution"]["rating"]
-            # print("rating distribution :", rating_distribution)
             review_count = 0
             for rating in rating_distribution:
                 review_count += rating["count"]
         except:
             pass
-        
+        # call finnal api :  <model_number> /availability?sitePath=us&region=null
         sizeLink =  apiUrl + productId + '/availability?sitePath=us&region=null'
         size_response = requests.request("GET", sizeLink , headers=headers)
         size_json_data = json.loads(size_response.text)
-        size_avail = size_json_data["variation_list"]
-        for size in size_avail:
-            if size["availability_status"] != 'NOT_AVAILABLE':
-                all_size.append(size["size"])
+        try:
+            size_avail = size_json_data["variation_list"]
+            for size in size_avail:
+                if size["availability_status"] != 'NOT_AVAILABLE':
+                    all_size.append(size["size"])
+        except:
+            size_avail = None
+        yield_data.append(product_id)
+        yield_data.append(model_number)
+        yield_data.append(product_name)
+        yield_data.append(standard_price)
+        yield_data.append(current_price)
+        yield_data.append(standard_price_no_vat)
+        yield_data.append(description_usps)
+        yield_data.append(product_color)
+        yield_data.append(product_category)
+        yield_data.append(brand)
+        yield_data.append(gender)
+        yield_data.append(product_type)
+        yield_data.append(over_all_rating)
+        yield_data.append(recommendation_percentage)
+        yield_data.append(review_count)
+        yield_data.append(description_title)
+        yield_data.append(description_subtitle)
+        yield_data.append(description_text)
+        yield_data.append(image_url)
+        yield_data.append(all_size)
+        yield_data.append(time.asctime(time.localtime(time.time())))
+        # print(yield_data)
+        write_data_to_csv(yield_data)
+        yield_data = []
         
-        print('------------------------------------------------------------------')
-        print("Scraping            No." ,product_number ," product")
-        print("* get time              :",time.asctime( time.localtime(time.time())))
-        print("* id                    :", product_id)
-        print("* model_number          :",model_number)
-        print("* product name          :" , product_name)
-        print("* standard price        :", Standard_price)
-        print("* current price         :", current_price)
-        print("* standard price no vat :", standard_price_no_vat)
-        print("* material              :", description_usps)
-        print("* product color         :", product_color)
-        print("* product category      :", product_category)
-        print("* product brand         :", brand)
-        print("* prodocut Rate Link    :", prodocutRateLink)
-        print("* gender                :", gender)
-        print("* product type          :", product_type)
-        print("* average rating        :", over_all_rating)
-        print("* recommendation %      :", recommendation_percentage , "%")
-        print("* review number         :", review_count)
-        print("* product title         :", description_title)
-        print("* product subtitle      :", description_subtitle)
-        print("* description text      :", description_text)
-        print("* image url             :", image_url)
-        print("* all size              :", all_size)
-        product_number += 1
+def write_data_to_csv(yield_data):
+    print(yield_data)
+    file = open( './adidas_products_data.csv', 'a')
+    csvCursor = csv.writer(file)
+    logData = [yield_data]
+    csvCursor.writerows(logData)
+    file.close()
 
 if __name__ == "__main__":
+    file = open( './adidas_products_data.csv', 'w')
+    csvCursor = csv.writer(file)
+    csvHeader = ['id', 'model_number', 'name', 'standard_price', 'current_price'
+    , 'standard_price_no_vat', 'material', 'color', 'category', 'brand', 'gender'
+    , 'type', 'average_rate', 'recommendation_percentage', 'review_count', 'description_title'
+    , 'description_subtitle',  'description_text', 'image_url', 'all_size', 'get_data_time']
+    csvCursor.writerow(csvHeader)
+    file.close()
+
+    paserUrl =["https://www.adidas.com/us/women-new_arrivals", "https://www.adidas.com/us/men-new_arrivals"]
     ratingApi = 'https://www.adidas.com/api/models/' #  / ratings?sitePath=us&region=null
     apiUrl = 'https://www.adidas.com/api/products/'  # availibleApi: <model_number> /availability?sitePath=us&region=null
-    paserUrl =["https://www.adidas.com/us/women-new_arrivals", "https://www.adidas.com/us/men-new_arrivals"]
     params = {"start": 0}
     adidasusUrl = "https://www.adidas.com"
     headers = {
@@ -130,8 +140,10 @@ if __name__ == "__main__":
     for url in paserUrl:
         response = requests.request("GET", url, headers=headers, params=params)
         soup = BeautifulSoup(response.text,"html.parser")
+        # get product url from img tag
         results = soup.findAll("div",{"class":"image"})
+        # get website pages number
         pagesNumbers = soup.find("li",{"class":"paging-total"}).text.split()[1]
-        # print(pagesNumbers)
         productHrefs =  get_all_product_href(paserUrl = url)
-        paser_products(productHrefList = productHrefs ) 
+        product_data = paser_products(productHrefList = productHrefs ) 
+        
